271b32eaa3dce70e9201d10a34cde646d2e2b3c7
Henrik Andersson <handerss@tibco.com>
2023-08-28 15:45:07 +0200

    Init project

---
+++ AnalyzerProject/AddPotatoPrefixAnalyzer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace AnalyzerProject;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public class AddPotatoPrefixAnalyzer : DiagnosticAnalyzer
+{
+    public const string DiagnosticId = "POTATO1";
+
+    public static readonly DiagnosticDescriptor Descriptor = new(DiagnosticId, string.Empty, "The variable '{0}' lacks the 'potato' prefix.",
+        "Design", DiagnosticSeverity.Warning, isEnabledByDefault: true);
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Descriptor);
+
+    public override void Initialize(AnalysisContext context)
+    {
+        context.EnableConcurrentExecution();
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+
+        context.RegisterCompilationStartAction(comContext =>
+        {
+            comContext.RegisterSyntaxNodeAction(AnalyzeSyntaxNode, SyntaxKind.VariableDeclaration);
+        });
+    }
+
+    private void AnalyzeSyntaxNode(SyntaxNodeAnalysisContext context)
+    {
+        if (context.Node is VariableDeclarationSyntax variable)
+        {
+            var varName = variable.Variables[0].Identifier.Text;
+            if (!varName.StartsWith("potato"))
+            {
+                var diagnostic = Diagnostic.Create(Descriptor, context.Node.GetLocation(), varName);
+                context.ReportDiagnostic(diagnostic);
+            }
+        } 
+    }
+}
---
+++ AnalyzerProject/AddPotatoPrefixCodeFixProvider.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace AnalyzerProject;
+
+[ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(AddPotatoPrefixAnalyzer)), Shared]
+public class AddPotatoPrefixCodeFixProvider : CodeFixProvider
+{
+    private const string CodeFixTitle = "Add potato prefix";
+
+    public override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(AddPotatoPrefixAnalyzer.DiagnosticId);
+
+    public override async Task RegisterCodeFixesAsync(CodeFixContext context)
+    {
+        var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+        var diagnostic = context.Diagnostics.First();
+        var diagnosticSpan = diagnostic.Location.SourceSpan;
+        var node = root.FindToken(diagnosticSpan.Start).Parent;
+        var variable = node.AncestorsAndSelf().OfType<VariableDeclarationSyntax>().First();
+
+        context.RegisterCodeFix(
+            CodeAction.Create(title: CodeFixTitle, createChangedDocument: c => AddPotatoPrefix(context.Document, variable, c), equivalenceKey: CodeFixTitle), 
+            diagnostic);
+    }
+
+    private static async Task<Document> AddPotatoPrefix(Document document, VariableDeclarationSyntax variable, CancellationToken c)
+    {
+        var syntaxRoot = await document.GetSyntaxRootAsync(c);
+        var newVariable = variable.Variables[0];
+        newVariable = newVariable.WithIdentifier(SyntaxFactory.Identifier($"potato{newVariable.Identifier.Text}"));
+        syntaxRoot = syntaxRoot.ReplaceNode(variable, variable.WithVariables(SyntaxFactory.SeparatedList(new [] { newVariable })));
+        return document.WithSyntaxRoot(syntaxRoot);
+    }
+
+    public override FixAllProvider GetFixAllProvider()
+    {
+        return WellKnownFixAllProviders.BatchFixer;
+    }
+}
